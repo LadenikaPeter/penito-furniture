271a9e7be8308b2d9d74a8a7f2a48b9260d9c1ed
peter <peterladenika@gmail.com>
2024-01-12 23:36:23 +0100

    imafe fix

--- src/constants/Home.js
+++ src/constants/Home.js
@@ -16,61 +16,61 @@ export const SECOND__SECTION = {
     {
       id: "i1",
       title: "Home Wooden Leg Chair",
-      src: "src/assets/items/item1.png",
+      src: "/assets/items/item1.png",
       alt: "chair",
     },
     {
       id: "i2",
       title: "Sofa Wooden Leg Chair",
-      src: "src/assets/items/item2.png",
+      src: "/assets/items/item2.png",
       alt: "sofa",
     },
     {
       id: "i3",
       title: "Home Wooden Leg Table",
-      src: "src/assets/items/item3.png",
+      src: "/assets/items/item3.png",
       alt: "table",
     },
     {
       id: "i4",
       title: "Plastic Chair",
-      src: "src/assets/items/item4.png",
+      src: "/assets/items/item4.png",
       alt: "plastic-chair",
     },
     {
       id: "i5",
       title: "Wooden Room Light",
-      src: "src/assets/items/item5.png",
+      src: "/assets/items/item5.png",
       alt: "light",
     },
     {
       id: "i6",
       title: "Couch Sofa",
-      src: "src/assets/items/item6.png",
+      src: "/assets/items/item6.png",
       alt: "couch-sofa",
     },
     {
       id: "i7",
       title: "LoungeChair",
-      src: "src/assets/items/item7.png",
+      src: "/assets/items/item7.png",
       alt: "lounge-chair",
     },
     {
       id: "i8",
       title: "Foot Rest Couch",
-      src: "src/assets/items/item8.png",
+      src: "/assets/items/item8.png",
       alt: "foot-rest-couch",
     },
     {
       id: "i9",
       title: "Room Table Lamp",
-      src: "src/assets/items/item9.png",
+      src: "/assets/items/item9.png",
       alt: "lamp",
     },
     {
       id: "i10",
       title: "Garden Table",
-      src: "src/assets/items/item10.png",
+      src: "/assets/items/item10.png",
       alt: "garden-table",
     },
   ],
@@ -80,13 +80,13 @@ export const SERVICES = [
   {
     first_set: [
       {
-        Image: "src/assets/delivery.svg",
+        Image: "/assets/delivery.svg",
         title: "Fast & Free Shipping",
         subHeading:
           "Sign up to premier delivery in Nigeria for unlimited free shipping from USA.",
       },
       {
-        Image: "src/assets/24hrs.svg",
+        Image: "/assets/24hrs.svg",
         title: "24/7 Support",
         subHeading:
           "24/7 support is a kind of support that is available throughout.",
@@ -96,13 +96,13 @@ export const SERVICES = [
   {
     second_set: [
       {
-        Image: "src/assets/repeat.svg",
+        Image: "/assets/repeat.svg",
         title: "Hassle Free Returns",
         subHeading:
           "Free return within 15 days for official store items and 7 days for other eligible items.",
       },
       {
-        Image: "src/assets/price.svg",
+        Image: "/assets/price.svg",
         title: "Your Best Price Matching",
         subHeading:
           "Ask to speak to a manger, show him the competitor’s lower price.",
